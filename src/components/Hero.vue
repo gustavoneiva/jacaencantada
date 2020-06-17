83cfbda3c5c8ecf5fc7fc573c4d18f85a227bdba
Luiz Neiva <luizgzn@gmail.com>
2020-06-16 22:55:04 -0300

    hero and navbar components

---
+++ src/components/Hero.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <section class="hero is-fullheight has-background">
+      <img class="hero-background" src="https://lh3.googleusercontent.com/pw/ACtC-3fhJV6fnONp9hkC0621annrlo7nZhLWf4nK9lchyM3tS-o0G4nN9qli5FujHAfhbKoIRofEIEJxlTJ7r5-YRBvHR-wH-C-CJIWFEzcvd6PBgp_gerLQxvxIXev6UwZbZzjol7VIlZnC4twf7VUS9z_vLw=w2394-h1596-no?authuser=0" alt="">
+      <div class="hero-head">
+      </div>
+      <div class="hero-body">
+          <div class="container has-text-centered">
+              <div class="column is-6 is-offset-3">
+                  <h1 class="title">
+                      Em construção
+                  </h1>
+                  <h2 class="subtitle">
+                      Jaca direto do Vale Encantando.
+                  </h2>
+                  <div class="box">
+                      <div class="field is-grouped">
+                          <p class="control is-expanded">
+                              <input class="input" type="text" placeholder="Deixe seu email">
+                          </p>
+                          <p class="control">
+                              <a class="button is-info">
+                                  Quero saber
+                              </a>
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Hero'
+}
+</script>
+
+<style scoped>
+.hero-has-background {
+  position: relative;
+  overflow: hidden;
+}
+.hero-background {
+  position: absolute;
+  object-fit: cover;
+  object-position: center center;
+  width: 100%;
+  height: 100%;
+  opacity: 0.67;
+}
+.title {
+  color: #17301C;
+  font-size:  5rem;
+}
+.subtitle {
+  color: #17301C;
+  font-size:  2rem;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="../">
+                JACA ENCANTADA
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+        </div>
+        <div id="navbarMenu" class="navbar-menu">
+            <div class="navbar-end">
+                <span class="navbar-item">
+                </span>
+            </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.navbar{
+  background-color: #004F2D;
+
+}
+.navbar-item {
+  color: #EAD637;
+}
+</style>
